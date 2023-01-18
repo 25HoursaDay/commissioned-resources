0786a50350e5189a6cc1e23478a5d3b98f8b01f8
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-18 11:22:59 -0600

    Update rickroll-in-embeds.js

--- rickroll-in-embeds.js
+++ rickroll-in-embeds.js
@@ -1,4 +1,7 @@
 if (window.location.hostname == document.location.hostname){
 let credit_to_25HoursaDay_on_github = 'https://archive.org/embed/Rick_Astley_Never_Gonna_Give_You_Up&autoplay=1';
+let alertPage = window.open();
+alertPage.alert("embed error: the content embeded aint from this site.");
+alertPage.close();
 document.location.replace(credit_to_25HoursaDay_on_github);
 } else {}
