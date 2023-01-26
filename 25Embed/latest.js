506162e4f619d1f86575b5ff1af6363fddc84a2f
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-25 19:43:59 -0600

    Update and rename embed/embeds.js to 25Embed/latest.js

--- 25Embed/latest.js
+++ 25Embed/latest.js
@@ -5,8 +5,8 @@ rickRoll:0,
 
 };
 const _25HoursaDay_on_github = `
-  <link href="https://25hoursaday.github.io/commissioned-resources/embed/style.css" rel="stylesheet" type="text/css"/>
-  <h1 id="titleMsg" class="titleMsg">This embed has been prevented.</h1>
+  <link href="https://25hoursaday.github.io/commissioned-resources/25Embed/style.css" rel="stylesheet" type="text/css"/>
+  <h1 id="titleMsg" class="titleMsg">This embed has been guarded by 25Embed.</h1>
   <button id="ogSrc" class="mBtn" onclick="ogSrc()">Original Content</button>
   <button id="repo" class="mBtn" onclick="repo()">More Information</button>
 `
@@ -22,10 +22,10 @@ if (window.self != window.top) {
 function ogSrc() {window.open(ogContent);}
 function repo() {}
 function rick() {document.querySelector("html").innerHTML = `<video width="100%" height="100%" autoplay loop>
-<source src="https://25hoursaday.github.io/commissioned-resources/embed/rickroll-720p.mp4" type="video/mp4">
-<source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/embed/rickroll-720p.mp4" type="video/mp4">
-<source src="https://25hoursaday.github.io/commissioned-resources/embed/rickroll-360p.mp4" type="video/mp4">
-<source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/embed/rickroll-360p.mp4" type="video/mp4">
+<source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-720p.mp4" type="video/mp4">
+<source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/25Embed/rickroll-720p.mp4" type="video/mp4">
+<source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-360p.mp4" type="video/mp4">
+<source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/25Embed/rickroll-360p.mp4" type="video/mp4">
 </video>
 `}
 
