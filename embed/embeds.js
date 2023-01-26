a7e481f66b2acd53862c1e5d5cfed378fcae71de
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-25 18:31:53 -0600

    Update embeds.js

--- embed/embeds.js
+++ embed/embeds.js
@@ -1,13 +1,19 @@
+// 0 is disabled, 1 is enabled
+
+const _25embedOptions = {
+rickRoll:0,
+
+};
 const _25HoursaDay_on_github = `
-  <style></style>
-  <h1>This embed prevented.</h1><br>
-  <button onclick="ogSrc()">Original Content</button>
-  <button onclick="repo()">What is this screen?</button>
+  <style>html {text-align: center;font-weight:bold;font-family:sans serif;background-color:#000000;color:blue;} button {border: 4px solid blue; border-radius:20px; color:blue; background-color:black; padding:10px; font-size:20px;font-weight:bold;box-shadow:blue 0px 0px 15px}</style>
+  <h1>This embed has been prevented.</h1>
+  <button id="ogSrc" onclick="ogSrc()">Original Content</button>
+  <button id="repo" onclick="repo()">More Information</button>
 `
 
 if (window.self != window.top) {
   if (!document.referrer.includes(self.location)) {
-      if (embedOptions.rickRoll == 1) {rick()} else {
+      if (_25embedOptions.rickRoll == 1) {rick()} else {
     var ogContent = window.self;
     document.querySelector("html").innerHTML = _25HoursaDay_on_github;
     }
@@ -24,9 +30,3 @@ function rick() {document.querySelector("html").innerHTML = `<video width="100%"
 </video>
 `}
 
-// 0 is disabled, 1 is enabled
-
-const embedOptions = {
-rickRoll:0,
-
-};
