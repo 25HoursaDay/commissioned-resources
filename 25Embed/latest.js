22558c69329f1e218d1518e4b2b1b401571d2445
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-25 20:40:22 -0600

    Update latest.js

--- 25Embed/latest.js
+++ 25Embed/latest.js
@@ -4,9 +4,8 @@ const _25embedOptions = {
 rickRoll:0,
 allowSameHost:1
 };
-const _25embedAllowedUrls = {
+const _25embedAllowedUrls = {}
 
-}
 const _25HoursaDay_on_github = `
   <link href="https://25hoursaday.github.io/commissioned-resources/25Embed/style.css" rel="stylesheet" type="text/css"/>
   <h1 id="titleMsg" class="titleMsg">This embed has been guarded by 25Embed.</h1>
@@ -15,9 +14,13 @@ const _25HoursaDay_on_github = `
 `
 // check if the source content is NOT the top element
 if (window.self != window.top) {
-  // check if the referrer of the page is the owner of the source content or not
+    console.log("window.self != window.top");
+  
+// check if the referrer of the page is the owner of the source content or not
   if (!document.referrer.includes(self.location)) {
-    // check if the source content owner is NOT embedding the source content and check if the "allowSameHost" setting is enabled if it is the content owner
+      console.log("!document.referrer.includes(self.location)");
+    
+// check if the source content owner is embedding the source content and if so, check if the "allowSameHost" setting is enabled. 
     if (window.self.location.hostname == window.top.location.hostname) {if (_25embedOptions.allowSameHost == 1) {var contentOwner = true;} else {var contentOwner = false;}            
       } else {var contentOwner = false;}
     } else {var contentOwner = true;}
@@ -30,6 +33,7 @@ if (contentOwner != true) {
     if (_25embedOptions.rickRoll == 1) {rick();} else {var ogContent = window.self; document.querySelector("html").innerHTML = _25HoursaDay_on_github;}
   }
 }
+
 function ogSrc() {window.open(ogContent);}
 function repo() {}
 function rick() {document.querySelector("html").innerHTML = `<video width="100%" height="100%" autoplay loop>
