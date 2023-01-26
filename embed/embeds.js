1e1773627207e4d80ba3617c40f95d91fbe8a5ad
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-25 19:31:19 -0600

    Update embeds.js

--- embed/embeds.js
+++ embed/embeds.js
@@ -5,28 +5,10 @@ rickRoll:0,
 
 };
 const _25HoursaDay_on_github = `
-  <style>
- html {
-  text-align:center;
-  font-weight:bold;
-  font-family:sans serif;
-  background-color:#000000;
-  color:blue;
-  } 
- button {
-  border:4px solid blue;
-  border-radius:20px;
-  color:blue;
-  background-color:black;
-  padding:10px;
-  font-size:20px;
-  font-weight:bold;
-  box-shadow:blue 0px 0px 15px
-  }
-  </style>
-  <h1>This embed has been prevented.</h1>
-  <button id="ogSrc" onclick="ogSrc()">Original Content</button>
-  <button id="repo" onclick="repo()">More Information</button>
+  <link href="https://25hoursaday.github.io/commissioned-resources/embed/style.css" rel="stylesheet" type="text/css"/>
+  <h1 id="titleMsg" class="titleMsg">This embed has been prevented.</h1>
+  <button id="ogSrc" class="mBtn" onclick="ogSrc()">Original Content</button>
+  <button id="repo" class="mBtn" onclick="repo()">More Information</button>
 `
 
 if (window.self != window.top) {
