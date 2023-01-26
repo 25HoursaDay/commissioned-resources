bcff0b8fa74f58d1e81f633b7697eac0aa0e60fa
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-26 08:35:20 -0600

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -1,7 +1,8 @@
 // 0 is disabled, 1 is enabled
 const _25embedOptions = {
-rickRoll:0,
-allowSameHost:1
+rickRoll:0, // default set to 0
+allowSameHost:1, // default set to 1
+allowFileUrls:0 // default set to 0
 };
 
 const _25embedAllowedUrls = {};
