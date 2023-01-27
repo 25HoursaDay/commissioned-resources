c70ffd92e75797b8ce40860f8cba81f40f3be107
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-27 16:59:06 -0600

    Update latest.js

--- 25Embed/latest.js
+++ 25Embed/latest.js
@@ -31,14 +31,14 @@ function _25embedLaunch() {
     if (contentOwner != "y") {
       if (contentOwner == "n") {
         if (_25embedOptions.rickRoll == "n") {
-          document.write(_25HoursaDay_on_github);
+          document.innerHTML = _25HoursaDay_on_github;
         } else {rick();}
       }
     }
 }
 function ogSrc() {window.open(ogContent);}
 function repo() {window.open("https://github.com/25HoursaDay/commissioned-resources/blob/main/25Embed/README.md#about-25embed");}
-function rick() {document.write(`<video width="100%" height="100%" autoplay loop>
+function rick() {document.innerHTML = `<video width="100%" height="100%" autoplay loop>
 <source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-720p.mp4" type="video/mp4">
 <source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/25Embed/rickroll-720p.mp4" type="video/mp4">
 <source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-360p.mp4" type="video/mp4">
