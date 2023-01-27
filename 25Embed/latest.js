a6cb742349ff6c2fafdecc386b59d90e5eb6aa3a
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-27 12:05:41 -0600

    Update latest.js

--- 25Embed/latest.js
+++ 25Embed/latest.js
@@ -11,31 +11,32 @@ var ogContent = window.self.location;
 var contentOwner = "unset";
 
 // file protection
-if (_25embedOptions.allowFileHost == "e") {} else {
-  
-if (window.top.location.startsWith("file:") && contentOwner = "unset") {var contentOwner = "no";  var reason = "Content is embedded on a file URL.";} else {}  
-  
-if (window.self.location.startsWith("file:") && contentOwner = "unset") {var contentOwner = "no"; var reason = "Content is hosted on a file URL.";} else {}
+if (_25embedOptions.allowFileHost == "n" && contentOwner = "unset") {
+  if (window.location.startsWith("file:") && contentOwner = "unset") {
+    var contentOwner = "n"; var reason = "Content is hosted on a file URL.";
+  }
 }
 
 // check if the source content is NOT the top element
-if (window.self != window.top && contentOwner = "no") {
+if (window.self != window.top && contentOwner = "unset") {
   
 // check if the referrer of the page is NOT the owner of the source content
-  if (!document.referrer.includes(self.location) && contentOwner = "no") {
+  if (!document.referrer.includes(self.location) && contentOwner = "unset") {
     
 // check if the source content owner is embedding the source content and if so, check if the "allowSameHost" setting is enabled. 
-    if (window.self.location.hostname == window.top.location.hostname && contentOwner = "no") {
-      if (_25embedOptions.allowSameHost == "e") {var contentOwner = "no"; var reason = "Same host, different URL.";} else {var contentOwner = "yes";}
-      check();
-      } else {var reason = "The embedded content does not belong to this website.";}
-    } else {var contentOwner = "yes";}
-  } else {var contentOwner = "yes";}
+    if (window.self.location.hostname == window.top.location.hostname && contentOwner = "n") {
+      if (_25embedOptions.allowSameHost == "y") {
+        var contentOwner = "unset"; var reason = "Same host, different URL.";} else {var contentOwner = "y";
+      }
+      
+    } else {var reason = "The embedded content does not belong to this website.";}
+  } else {var contentOwner = "y";}
+} else {var contentOwner = "y";}
 
 
-  if (contentOwner != "yes") {
-    if (contentOwner = "no") {
-      if (_25embedOptions.rickRoll == "e") {rick();} else {document.write(_25HoursaDay_on_github);}
+  if (contentOwner != "y") {
+    if (contentOwner = "n") {
+      if (_25embedOptions.rickRoll == "y") {rick();} else {document.write(_25HoursaDay_on_github);}
     }
   }
 function ogSrc() {window.open(ogContent);}
