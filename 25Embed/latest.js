c68eb755d97dca1fb69ad4a68ca0e971be49eb2e
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-27 11:21:33 -0600

    Update latest.js

--- 25Embed/latest.js
+++ 25Embed/latest.js
@@ -1,4 +1,4 @@
-const _25HoursaDay_on_github = `
+var _25HoursaDay_on_github = `
   <link href="https://25hoursaday.github.io/commissioned-resources/25Embed/style.css" rel="stylesheet" type="text/css"/>
   <h1 id="titleMsg" class="titleMsg">This content has been guarded by 25Embed.</h1>
   <h2 id="reason" class="reason">Reason: '`+reason+`'</h2>
@@ -6,42 +6,40 @@ const _25HoursaDay_on_github = `
   <button id="repo" class="mBtn" onclick="repo()">More Information</button>
 `;
 
-var override = false;
+var override = "no";
 var reason = "Prevented";
 var ogContent = window.self.location;
+var contentOwner = "unset";
 
 // file protection
-if (_25embedOptions.allowFileHost == 1) {} else {
+if (_25embedOptions.allowFileHost == "e") {} else {
   
-if (window.top.location.startsWith("file:") && override = false) {var contentOwner = false; var override = true; var reason = "Content is embedded on a file URL."; console.log("_25embedOptions.allowFileHost == 0");} else {}  
+if (window.top.location.startsWith("file:") && contentOwner = "unset") {var contentOwner = false; var override = true; var reason = "Content is embedded on a file URL.";} else {}  
   
-if (window.self.location.startsWith("file:") && override = false) {var contentOwner = false; var override = true; var reason = "Content is hosted on a file URL."; console.log("_25embedOptions.allowFileHost == 0");} else {}
+if (window.self.location.startsWith("file:") && contentOwner = "unset") {var contentOwner = false; var override = true; var reason = "Content is hosted on a file URL.";} else {}
 }
 
 // check if the source content is NOT the top element
 if (window.self != window.top && override = false) {
-    console.log("window.self != window.top");
   
 // check if the referrer of the page is NOT the owner of the source content
   if (!document.referrer.includes(self.location) && override = false) {
-      console.log("!document.referrer.includes(self.location)");
     
 // check if the source content owner is embedding the source content and if so, check if the "allowSameHost" setting is enabled. 
     if (window.self.location.hostname == window.top.location.hostname && override = false) {
-      if (_25embedOptions.allowSameHost == 0) {var contentOwner = false; var reason = "Same host, different URL."; console.log("_25embedOptions.allowSameHost == 0");} else {var contentOwner = true; var override = true;}
-      } else {var reason = "The embedded content does not belong to this website."; console.log("window.self != window.top && !document.referrer.includes(self.location)");}
+      if (_25embedOptions.allowSameHost == 0) {var contentOwner = false; var reason = "Same host, different URL.";} else {var contentOwner = true; var override = true;}
+      check();
+      } else {var reason = "The embedded content does not belong to this website.";}
     } else {var contentOwner = true; var override = true;}
   } else {var contentOwner = true; var override = true;}
 
-
-if (contentOwner != true) {
-  console.log("contentOwner != true");
-  if (contentOwner = false) {
-    console.log("contentOwner = false");
-    if (_25embedOptions.rickRoll == 1) {rick();} else {document.write(_25HoursaDay_on_github);}
+function check() {
+  if (contentOwner != true) {
+    if (contentOwner = false) {
+      if (_25embedOptions.rickRoll == "e") {rick();} else {document.write(_25HoursaDay_on_github);}
+    }
   }
 }
-
 function ogSrc() {window.open(ogContent);}
 function repo() {window.open("https://github.com/25HoursaDay/commissioned-resources/blob/main/25Embed/README.md#about-25embed");}
 function rick() {document.write(`<video width="100%" height="100%" autoplay loop>
@@ -50,6 +48,6 @@ function rick() {document.write(`<video width="100%" height="100%" autoplay loop
 <source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-360p.mp4" type="video/mp4">
 <source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/25Embed/rickroll-360p.mp4" type="video/mp4">
 </video>
-<script>console.log("rickroll deployed");</`+`script>`
+`
 );}
 
