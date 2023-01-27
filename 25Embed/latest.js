59abd72560c6a265c76339aefae63d4319df4c68
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-01-27 16:59:25 -0600

    Update latest.js

--- 25Embed/latest.js
+++ 25Embed/latest.js
@@ -1,7 +1,7 @@
 var _25HoursaDay_on_github = `
   <link href="https://25hoursaday.github.io/commissioned-resources/25Embed/style.css" rel="stylesheet" type="text/css"/>
   <h1 id="titleMsg" class="titleMsg">This content has been guarded by 25Embed.</h1>
-  <h2 id="reason" class="reason">Reason: '`+reason+`'</h2>
+  <h2 id="reason" class="reason">Reason: '`+ reason +`'</h2>
   <button id="ogSrc" class="mBtn" onclick="ogSrc()">Original Content</button>
   <button id="repo" class="mBtn" onclick="repo()">More Information</button>
 `;
