1977ac8d81c62aeef0f06599ff270428724a18d5
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-02-10 11:42:31 -0600

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -4,7 +4,6 @@ rickRoll:"n", // default set to n
 allowSameHost:"n", // default set to y
 allowFileHost:"n", // default set to n
 allowDataHost:"y", // default set to y
-allowJavascriptUrn:"y", // default set to y
 allowBlobHost:"y" // default set to y
 };
 // urls allowed to embed content
