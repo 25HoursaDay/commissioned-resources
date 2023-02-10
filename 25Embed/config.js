ca434634be704334f9b0cbe89d0284b4b8f0bf6e
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-02-10 13:50:20 -0600

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -8,7 +8,7 @@ allowBlobHost:"y" // default set to y
 };
 
 
-const _25embedWhitelistDomains = {"25HoursaDay.github.io","home-schoology.github.io"}; // domains allowed to embed content, defaulted to "25HoursaDay.github.io","home-schoology.github.io"
+const _25embedWhitelistDomains = {}; // domains allowed to embed content, defaulted to blank
 const _25embedWhitelistUrls = {}; // urls allowed to embed content, defaulted to none
 const _25embedBlacklistDomains = {}; // domains not allowed to embed content, defaulted to none
 const _25embedBlacklistUrls = {}; // urls not allowed to embed content, defaulted to none
