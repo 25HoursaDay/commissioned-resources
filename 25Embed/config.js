319c24673b13487a2e3c7b082163cfee62ba8ed1
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-02-13 08:48:18 -0600

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -1,5 +1,17 @@
 const _25embed = {
-  
+  options: {
+    
+    rickRoll:"n", // default set to n
+    allowSameHost:"n", // default set to y
+    allowFileHost:"n", // default set to n
+    allowDataHost:"y", // default set to y
+    allowBlobHost:"y", // default set to y
+    
+    domain: {
+      whitelist: {"25HoursaDay.github.io","home-schoology.github.io"},
+      blacklist: {}
+    }
+  }
 }
 
 
@@ -13,7 +25,5 @@ allowBlobHost:"y" // default set to y
 };
 
 
-const _25embedWhitelistDomains = {}; // domains allowed to embed content, defaulted to blank
-const _25embedWhitelistUrls = {}; // urls allowed to embed content, defaulted to none
+const _25embedWhitelistDomains = {"25HoursaDay.github.io","home-schoology.github.io"}; // domains allowed to embed content, defaulted to blank
 const _25embedBlacklistDomains = {}; // domains not allowed to embed content, defaulted to none
-const _25embedBlacklistUrls = {}; // urls not allowed to embed content, defaulted to none
