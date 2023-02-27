7f74973163444b0ff8644902cb62615a440582a8
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-02-27 08:42:00 -0600

    Create rick.js

---
+++ 25Embed/beta/rick.js
@@ -0,0 +1,19 @@
+
+
+
+function _25embedLaunch() {
+    if (contentOwner != "y") {
+        if (contentOwner == "n") {
+          if (_25embedOptions.rickRoll == "y") {rick();} 
+          else {_25HoursaDay_on_github(reason);}
+        } else {reason = "Content owner is undefined, contact content owner."; _25HoursaDay_on_github(reason);}
+    }
+}
+
+function rick() {document.write(`<video width="100%" height="100%" autoplay loop>
+<source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-720p.mp4" type="video/mp4">
+<source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/25Embed/rickroll-720p.mp4" type="video/mp4">
+<source src="https://25hoursaday.github.io/commissioned-resources/25Embed/rickroll-360p.mp4" type="video/mp4">
+<source src="https://cdn.jsdelivr.net/gh/25HoursaDay/commissioned-resources@main/25Embed/rickroll-360p.mp4" type="video/mp4">
+</video>
+`);}
