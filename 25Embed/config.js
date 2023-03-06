1eb7cda9cef16202f30695b4d93d1304fa236ffd
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-03-06 14:39:38 -0600

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -1,11 +1,12 @@
 // n is disabled, y is enabled
 const _25embedOptions = {
 rickRoll:"n", // default set to n
-allowSameHost:"n", // default set to y
+allowSameHost:"y", // default set to y
 allowFileHost:"n", // default set to n
 allowDataHost:"y", // default set to y
 allowBlobHost:"y", // default set to y
-supportDevs:"y" // default set to y
+supportDevs:"y", // default set to y
+ultraThreatProtection:"n" // default set to n because it has the ability to break everything
 };
 
 
