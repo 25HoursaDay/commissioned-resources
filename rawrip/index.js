bbc3e7327482c75d1b7b0f5b6b6b3b08f4cf2b46
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-03-22 12:17:20 -0500

    Update index.js

--- rawrip/index.js
+++ rawrip/index.js
@@ -1,5 +1,10 @@
 // dont forget to add type="module" to your script tag
   let rawParams = new URLSearchParams(document.currentScript.src.search);
   let exportURL = rawParams.get("exportURL");
+
+fetch(decodeURIComponent(exportURL))
+  .then((result) => { return result.text();})
+  .then((exportCode) => { exportCode; }); 
+  
   export {decodeURIComponent(exportURL)};
   export {decodeURIComponent(exportCode)};
