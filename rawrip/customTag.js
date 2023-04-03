65081e822363e3c142ada26635ffbc66efd9350d
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-04-03 09:40:35 -0500

    Update customTag.js

--- rawrip/customTag.js
+++ rawrip/customTag.js
@@ -1,11 +1,21 @@
 class HelloWorld extends HTMLElement {
+
     constructor() {
         super()
     }
+// custom callbacks start
+    connectedCallback() {
+        if (this.hasAttribute("ishidden")) {
+            this.style.backgroundColor = "grey"
+            this.style.pointerEvents = "none"
+        }
+    }
+// custom callbacks end
 // custom methods start
     getId() {
         return this.id
     }
-  // custom methods end
+// custom methods end
 }
+
 customElements.define("hello-world", HelloWorld)
