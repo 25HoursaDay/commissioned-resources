86c78a601da0a7c962a83c67ac7f5a267923f299
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-04-14 12:15:59 -0500

    Update index.js

--- rawrip/index.js
+++ rawrip/index.js
@@ -1,11 +1,12 @@
 // does not import modules to my knowledge
 // example of a valid url: https://example.com/?rawRipURL=
-// put script tag in containing element  
-  let rawRipars = new URLSearchParams(document.currentScript.src.search);
-  let rawRippedURL = atob(rawRipars.get("rawRipURL"));
+// put script tag in containing element
+if (document.currentScript) {
+  let rawRiparams = new URLSearchParams(document.currentScript.src.search);
+  let rawRippedURL = atob(rawRiparams.get("rawRipURL"));
   let thisRawScript = doument.currentScript;
   let rawTarget = getAttribute("rawRipTarget");
-
+} else {}
 function htmlRip() {
   fetch(rawRippedURL)
   .then((result) => {return result.text();})
