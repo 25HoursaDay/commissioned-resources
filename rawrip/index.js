ad3905b638fc84bfcf0ff1412f9a517243c4156b
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-04-14 11:17:49 -0500

    Update index.js

--- rawrip/index.js
+++ rawrip/index.js
@@ -2,7 +2,6 @@
 // put script tag in containing element  
   let rawRipars = new URLSearchParams(document.currentScript.src.search);
   let rawRippedURL = atob(rawRipars.get("rawRipURL"));
-  let rawRipVars = atob(rawRipars.get("rawRipVars"));
   let thisRawScript = doument.currentScript;
 
 function htmlRip() {
