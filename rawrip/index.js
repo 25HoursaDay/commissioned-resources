697821e24976a0ad95a596392217b8fd53a96dee
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-04-17 13:04:04 -0500

    Update index.js

--- rawrip/index.js
+++ rawrip/index.js
@@ -5,12 +5,12 @@ if (document.currentScript) {
   let rawRiparams = new URLSearchParams(document.currentScript.src.search);
   let rawRippedURL = atob(rawRiparams.get("rawRipURL"));
   let thisRawScript = doument.currentScript;
-  let rawTarget = getAttribute("rawRipTarget");
+  let ripLoc = getAttribute("ripLoc");
 
 function htmlRip() {
   fetch(rawRippedURL)
   .then((result) => {return result.text();})
-  .then((rippedCode) => {rawTarget.innerHTML = rippedCode;}); 
+  .then((rippedCode) => {ripLoc.innerHTML = rippedCode;}); 
 }
 htmlRip();
 } else {}
