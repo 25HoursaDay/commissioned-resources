80bfc7045fe01b19b3e178cdede237d5544e4785
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-16 10:43:51 -0500

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -1,16 +1,30 @@
 // n is disabled, y is enabled
 const _25embedOptions = {
-rickRoll:"n", // default set to n
-allowSameHost:"y", // default set to y
-allowFileHost:"n", // default set to n
-allowDataHost:"y", // default set to y
-allowBlobHost:"y", // default set to y
-supportDevs:"y", // default set to y
-onlyAllowHTTP:"n", // default set to n
-onlyAllowHTTPS:"n", // default set to n
-ultraThreatProtection:"n" // default set to n because it has the ability to break everything
+rickRoll:"n",
+allowSameHost:"y",
+allowFileHost:"n",
+allowDataHost:"y",
+allowBlobHost:"y",
+allowBlankHost:"n",
+supportDevs:"y",
+onlyAllowHTTP:"n",
+onlyAllowHTTPS:"n",
+ultraThreatProtection:"n"
 };
 
+/* DEFAULT SETTINGS: */
+/*
+rickRoll:"n",
+allowSameHost:"y",
+allowFileHost:"n",
+allowDataHost:"y",
+allowBlobHost:"y",
+allowBlankHost:"n",
+supportDevs:"y",
+onlyAllowHTTP:"n",
+onlyAllowHTTPS:"n",
+ultraThreatProtection:"n" <-because it has the ability to break everything
+*/
 
 const _25embedWhitelistDomains = ["25HoursaDay.github.io","home-schoology.github.io"]; // domains allowed to embed content, defaulted to blank
 const _25embedBlacklistDomains = []; // domains not allowed to embed content, defaulted to none
