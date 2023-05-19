13913781351090c0d44857b9120c60d77b0e16cb
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-19 10:29:15 -0500

    Update config.js

--- 25Embed/config.js
+++ 25Embed/config.js
@@ -1,5 +1,6 @@
+/* DEFAULT SETTINGS: */
 // n is disabled, y is enabled
-const _25embedDefaults = {
+const _25embedOptions = {
   allowSameHost: "y",
   allowFileHost: "n",
   allowDataHost: "y",
@@ -13,23 +14,7 @@ const _25embedDefaults = {
   ultraThreatProtection: "n"
 };
 const themeList = ["blue", "green"];
-// let theme = "blue";
-
-/* DEFAULT SETTINGS: */
-/*
-allowSameHost:"y",
-allowFileHost:"n",
-allowDataHost:"y",
-allowBlobHost:"y",
-allowBlankHost:"n",
-allowProxyHost:"y",
-supportDevs:"y",
-onlyAllowHTTP:"n",
-onlyAllowHTTPS:"n",
-rickRoll:"n",
-ultraThreatProtection:"n", <-because it has the ability to break everything
-theme: "blue"
-*/
+let theme = "blue";
 
 const _25embedWhitelistDomains = ["25HoursaDay.github.io", "home-schoology.github.io"]; // domains allowed to embed content, defaulted to blank
 const _25embedBlacklistDomains = []; // domains not allowed to embed content, defaulted to none
