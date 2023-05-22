6007acfcd917116972539ab262c94a0116cbdb13
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-22 10:49:57 -0500

    Update pain.js

--- 25Embed/beta/pain.js
+++ 25Embed/beta/pain.js
@@ -132,8 +132,10 @@ function _25HoursaDay_on_github(del, rsn) {
   <div id="_25embedScreen" class="_25embedScreen">
   <h1 id="titleMsg" class="titleMsg">This content has been guarded by 25Embed.</h1>
   <h2 id="reason" class="reason">Reason: '` + rsn + `'</h2>
+  <div class="rowClass">
   <button id="ogSrc" class="mBtn" onclick="ogSrc()">Original Content</button>
   <button id="repo" class="mBtn" onclick="repo()">More Information</button>
+  </div>
   <script>
   var ogContent = self.location;
   function ogSrc() {window.open(ogContent);}
