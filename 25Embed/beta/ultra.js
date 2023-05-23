d96c85cde6986dff76a44d434909c210ffbe8378
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-23 08:57:15 -0500

    Update ultra.js

--- 25Embed/beta/ultra.js
+++ 25Embed/beta/ultra.js
@@ -1,1 +1,9 @@
 // prevent webcache, archive.org, proxies, ect.
+
+if (_25embedOptions.ultraThreatProtection == "y") {
+  if (window.top.location.hostname == "webcache.googleusercontent.com/" || window.location.hostname == "webcache.googleusercontent.com/") {
+
+    window.close();
+    _25embedLaunch("n", 'Protocol "ultraThreatProtection"');
+  }
+}
