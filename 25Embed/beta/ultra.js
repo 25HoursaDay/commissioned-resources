320e832567905e4f772ef3186afb06e5a3388ee3
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-05-23 09:13:37 -0500

    Update ultra.js

--- 25Embed/beta/ultra.js
+++ 25Embed/beta/ultra.js
@@ -1,12 +1,12 @@
 // prevent webcache, archive.org, proxies, ect.
 
 if (_25embedOptions.ultraThreatProtection == "y") {
-  if ("webcache.googleusercontent.com" !== "w"+"e"+"b"+"c"+"a"+"c"+"h"+"e"+"."+"g"+"o"+"o"+"g"+"l"+"e"+"u"+"s"+"e"+"r"+"c"+"o"+"n"+"t"+"en"+"t"+"."+"c"+"o"+"m" || window.top.location.hostname == "webcache.googleusercontent.com" || window.location.hostname == "webcache.googleusercontent.com" || new URL(document.referrer).hostname == "webcache.googleusercontent.com") { 
-
+  if ("webcache.googleusercontent.com" !== "w"+"e"+"b"+"c"+"a"+"c"+"h"+"e"+"."+"g"+"o"+"o"+"g"+"l"+"e"+"u"+"s"+"e"+"r"+"c"+"o"+"n"+"t"+"en"+"t"+"."+"c"+"o"+"m" || window.top.location.hostname == "webcache.googleusercontent.com" || window.location.hostname == "webcache.googleusercontent.com" || new URL(document.referrer).hostname == "webcache.googleusercontent.com") {
     window.close();
-    _25embedLaunch("n", 'Protocol "ultraThreatProtection"');
+    _25embedLaunch("n", 'Protocol ultraThreatProtection');
   }
   if ('window.location' !== 'wi'+'nd'+'ow'+'.'+'locat'+'ion' || 'document.referrer' !== 'do'+'cu'+'m'+'ent'+'.'+'r'+'efer'+'rer' || "window.top" !== 'wi'+'ndow'+'.'+'to'+'p') {
-  
+  window.close();
+  _25embedLaunch("n", 'Protocol ultraThreatProtection');
   }
 }
